d5c277adea63c249b4eda9ea2e5df36905498918
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-06-12 09:35:27 -0700

    ratings input

--- src/app/add-park/page.tsx
+++ src/app/add-park/page.tsx
@@ -1,27 +1,41 @@
 'use client'
+import { useState } from "react";
 import { FormInput } from "@/components/form-components/Input";
 import {useSelectedParkStore} from "@/store/selected-park-store";
+import { RatingsInput } from "@/components/form-components/ratings-input";
+
+
 
 export default function AddParkPage() {
     const selectedPark = useSelectedParkStore(state => state.selectedPark);
+    
+    const [rating, setRating] = useState(0);
+
+    const handleRating = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setRating(Number(e.target.value));
+    };
+    
+    if (!selectedPark) return null;
+    // write an error boundry for this
     return (
-            <div className="w-full h-screen w-max-[960px] mx-auto py-5 flex flex-col items-start">
-                {selectedPark && (
-                    <div className="self-start">
-                        <h1 className="text-4xl">{selectedPark.address_components[0].long_name}</h1>
-                    </div>
-                )}
-                <div className=" w-11/12 py-4 mx-auto drop-shadow-md rounded-lg self-start">
-                    <form className="flex flex-col gap-2">
-                       <FormInput
-                            name="park-name"
-                            label="Park Name"
-                            type="text"
-                            value={selectedPark.address_components[0].long_name}
-                            id="park-name"
-                       />
-                    </form>
+        <div className="w-full h-screen w-max-[960px] mx-auto py-5 flex flex-col items-start">
+            {selectedPark && (
+                <div className="self-start">
+                    <h1 className="text-4xl">{selectedPark.address_components[0].long_name}</h1>
                 </div>
+            )}
+            <div className=" w-11/12 py-4 mx-auto drop-shadow-md rounded-lg self-start">
+                <form className=" w-11/12 p-2 flex flex-col gap-2 border-2 border-gray-300 rounded-md">
+                    <FormInput
+                        name="park-name"
+                        label="Park Name"
+                        type="text"
+                        value={selectedPark.address_components[0].long_name}
+                        id="park-name"
+                    />
+                    <RatingsInput rating={rating} setRating={() => handleRating} />
+                </form>
             </div>
+        </div>
     )
 }
---
+++ src/components/form-components/ratings-input.tsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+
+export const RatingsInput = ({rating, setRating}: {rating: number, setRating: React.Dispatch<React.SetStateAction<number>>}) => (
+    <div className="flex flex-col gap-2">
+        <div>
+            <label htmlFor="park-description">Park Rating</label>
+        </div>
+        <div className="flex gap-1">
+            {[...Array(5)].map((value) => (
+                <FontAwesomeIcon 
+                    icon={faStar} 
+                    key={value}
+                    color={value < 1 ? "yellow" : "gray"}
+                    onClick={() => setRating(value + 1)}
+                />
+            ))}
+        </div>
+    </div>
+);
