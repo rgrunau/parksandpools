01b72c534819564482d84d2e512b51f54cb853d8
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-06-14 08:57:02 -0700

    some smalll updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <div className="w-full lg:w-1/3">
             <h1 className="text-4xl text-pink-500">Parks and Pools</h1>
           </div>
-          <div>
+          <div className="justify-self-center">
             <Link 
               href="/park-lookup"
               className="bg-pink-500 text-white px-4 py-2 rounded-md"
