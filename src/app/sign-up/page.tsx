665b80e3d5c9406fe123847660ec8be8cff816f6
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-06-14 09:00:18 -0700

    basic style for auth pages

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="w-full flex items-center justify-center py-8">
+      <SignUp />
+    </div>
+  );
 }
