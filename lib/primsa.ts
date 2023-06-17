94f1db43ed8aeb3bafa6303be5baf4c1d8bfd03f
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-06-17 15:42:39 -0700

    get user data from database

--- lib/primsa.ts
+++ lib/primsa.ts
@@ -1,14 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient
+let prisma: PrismaClient;
 
-if(process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
 } else {
-    if(!global.prisma) {
-      global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
+    // @ts-ignore
+  if (!global.prisma) {
+    // @ts-ignore
+    global.prisma = new PrismaClient();
+  }
+    // @ts-ignore
+  prisma = global.prisma;
 }
 
-export default prisma
+export default prisma;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
 import DashboardCard from "@/components/home-components/DashboardCard";
 import Link from "next/link";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
+import prisma from "../../lib/primsa";
 
 
+async function getUserInfo(email: string | undefined) {
+  const userData = await prisma.user.findUnique({
+    where: {
+      email: email
+    },
+  })
+  return {
+    props: {userData},
+    revalidate: 10
+  }
+}
 
-export default function Home() {
+interface User {
+  id: number,
+  name: string,
+  email: string,
+  createdAt: Date,
+  updatedAt: Date,
+}
+
+export default async function Home() {
   const rencentParks = false
+  const loggedInuser = await currentUser()
+  // console.log(loggedInuser?.emailAddresses[0].emailAddress)
+  let user = await getUserInfo(loggedInuser?.emailAddresses[0].emailAddress)
+  console.log(user)
   return (
     <div className="h-screen w-screen flex justify-center p-8 bg-slate-200">
       <div className="w-full w-max-10/12 p-4 flex flex-col items-center bg-slate-50 rounded-lg">
@@ -27,26 +51,30 @@ export default function Home() {
           </div>
         </div>
         {/* dashboard body */}
-        <div className="w-10/12 flex flex-col lg:flex-row items-center justify-between my-4 p-4">
-          {/* dashboard card */}
-          <DashboardCard
-            cardHeaderText="Recent Parks"
-            renderCardBody={() => (
-              <div>
-                  {rencentParks && (
-                      <div className="flex flex-col gap-2">
-                      parks
-                      </div>
-                  )}
-                  {!rencentParks && (
-                      <div>
-                      <h2 className="text-xl text-slate-700">No Recent Parks</h2>
-                      <h3>Go to a park</h3>
-                      </div>
-                  )}
-              </div>
-            )}
-          />
+        <div className="w-10/12 flex flex-col items-center justify-between my-4 p-4">
+          <div className="w-full p-2">
+            <h1 className="text-4xl text-slate-700">Welcome {loggedInuser?.firstName}</h1>
+          </div>
+          <div className="w-full lg:w-1/2 self-start py-4 px-2">
+            <DashboardCard
+              cardHeaderText="Recent Parks"
+              renderCardBody={() => (
+                <div>
+                    {rencentParks && (
+                        <div className="flex flex-col gap-2">
+                        parks
+                        </div>
+                    )}
+                    {!rencentParks && (
+                        <div>
+                        <h2 className="text-xl text-slate-700">No Recent Parks</h2>
+                        <h3>Go to a park</h3>
+                        </div>
+                    )}
+                </div>
+              )}
+            />
+          </div>
         </div>
       </div>
     </div>
