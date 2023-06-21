d691cffd323099b4160b38d5e830564eb6699cc0
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-06-20 21:48:56 -0700

    adjust header

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,21 @@
 import DashboardCard from "@/components/home-components/DashboardCard";
-import Link from "next/link";
-import { UserButton,  } from "@clerk/nextjs";
+import DashboardHeader from "@/components/home-components/DashboardHeader";
 import prisma from "../../../lib/primsa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { UserButton } from "@clerk/nextjs";
 
 
 
 export default async function Dashboard() {
   let rencentParks = await prisma.visitedPark.findMany();
   return (
-    <div className="h-screen w-screen flex justify-center p-8 bg-slate-200">
-      <div className="w-full w-max-10/12 p-4 flex flex-col items-center bg-slate-50 rounded-lg">
-        {/* dashboard header */}
-        <div className="w-10/12 flex flex-col lg:flex-row items-center justify-between my-4 drop-shadow-xl rounded-md bg-white p-2">
-          <div className="w-full lg:w-1/3">
-            <h1 className="text-4xl text-pink-500">Parks and Pools</h1>
-          </div>
-          <div className="justify-self-center">
-            <Link 
-              href="/park-lookup"
-              className="bg-pink-500 text-white px-4 py-2 rounded-md"
-            >
-              Park Lookup
-            </Link>
-          </div>
-          <div>
-            <UserButton afterSignOutUrl="/"/>
-          </div>
+    <div className="h-screen w-screen flex justify-center lg:p-8 bg-slate-200">
+      <div className="w-full lg:w-10/12 p-4 flex flex-col items-center bg-slate-50 rounded-lg">
+        <div className="w-full flex items-center justify-end">
+          <UserButton />
         </div>
+        <DashboardHeader />
         {/* dashboard body */}
         <div className="w-10/12 flex flex-col items-center justify-between my-4 p-4">
           <div className="w-full lg:w-1/2 self-start py-4 px-2">
