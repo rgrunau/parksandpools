6567a5e02e9b06ed6fb1961a58f7ceba63603af1
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-06-24 13:54:27 -0700

    add empty state

--- src/app/parks/favorites/page.tsx
+++ src/app/parks/favorites/page.tsx
@@ -2,6 +2,9 @@ import prisma from "../../../../lib/primsa"
 import { getClerkUser } from "../../../../lib/getClerkUser"
 import { redirect } from "next/navigation";
 import ParksCard from "@/components/parks/components/park-cards";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 export default async function FavoritesPage() {
    const user = await getClerkUser();
    if (!user) {
@@ -19,6 +22,30 @@ export default async function FavoritesPage() {
             {favorites && favorites.map((favorite) => (
                <ParksCard key={favorite.parkId} park={favorite} />
             ))}
+            {favorites.length === 0 && (
+                <div className="flex flex-col items-center gap-3 text-center text-2xl">
+                    <div className="py-4 text-slate-800 font-semibold">
+                        You have no favorites yet!
+                    </div>
+                    <div className="w-[250px] py-4">
+                        <Link 
+                            className="text-slate-50 bg-pink-500 py-2 px-1 rounded-lg flex justify-center items-center gap-2"
+                            href="/park-lookup"
+                        >
+                            
+                            <div className="w-1/12">
+                                <FontAwesomeIcon 
+                                    className="text-slate-50"
+                                    icon={faPlus} 
+                                />
+                            </div>
+                            <div className="w-3/4">
+                                a favorite park
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
