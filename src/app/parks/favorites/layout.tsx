d91da99d63fc9aa3e383eb75a3aa65687dc64217
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-06-25 11:23:30 -0700

    most of the color adjustments

--- src/app/parks/favorites/layout.tsx
+++ src/app/parks/favorites/layout.tsx
@@ -4,8 +4,8 @@ export default function FavoritesLayout({children}: {children: React.ReactNode})
 
     return (
         <main className="flex flex-col px-6 py-8">
-            <div className="w-full text-left border-b-2 py-2 border-b-slate-900">
-                <h1 className="text-3xl font-semibold text-pink-500">Your Favorites Parks</h1>
+            <div className="w-full text-left border-b-2 py-2 border-b-primary-blue">
+                <h1 className="text-3xl font-semibold text-primary-red">Your Favorites Parks</h1>
             </div>
             <section className="w-full py-4">   
                 {children}
